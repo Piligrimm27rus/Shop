2dbb3808c47003f8592034680b19c85b81d56eac
Piligrimm27rus <cod400446@gmail.com>
2024-01-10 23:10:27 +0600

    added Ninject

--- src/Piligrimm.Server/Web/Program.cs
+++ src/Piligrimm.Server/Web/Program.cs
@@ -1,9 +1,5 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Ninject;
-using System.Reflection;
 using Ninject.Extensions.DependencyInjection;
+using Ninject;
 
 var builder = WebApplication.CreateBuilder(args);
 
